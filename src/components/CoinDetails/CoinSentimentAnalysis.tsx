c97cb2d7dbbd5bc98531d07cde167d0f46f0910b
Dipesh <55049408+dipeshrajput@users.noreply.github.com>
2023-05-17 16:57:10 +0530

    Update CoinSentimentAnalysis.tsx

--- src/components/CoinDetails/CoinSentimentAnalysis.tsx
+++ src/components/CoinDetails/CoinSentimentAnalysis.tsx
@@ -1,4 +1,4 @@
- import {
+import {
   TooltipProvider,
   TooltipTrigger,
   Tooltip,
@@ -6,8 +6,9 @@
 } from "@radix-ui/react-tooltip";
 import dynamic from "next/dynamic";
 import React, { useEffect, useState } from "react";
-import { VictoryChart, VictoryLine } from "victory";
+import { VictoryChart, VictoryLine, VictoryTheme } from "victory";
 import { InfoFilledIcon } from "../../../public/assets/icons";
+import { reverse } from "dns";
 
 const GaugeChart = dynamic(() => import("react-gauge-chart"), { ssr: false });
 
@@ -36,56 +37,42 @@ const CoinSentimentAnalysis = ({
       rsi?.value < 70 &&
       ma?.value < price
     ) {
+  
       setBuySellSignalStrength(0.8);
     } else if (
       macd[0]?.valueMACD < macd[0]?.valueMACDSignal &&
       rsi?.value > 30
     ) {
-      setBuySellSignalStrength(0.2);
-    } else if (macd[0]?.valueMACD > macd[0]?.valueMACDSignal && rsi?.value > 70) {
-      setBuySellSignalStrength(0.3);
+     
+       setBuySellSignalStrength(0.2);
+    }
+      else if (
+      macd[0]?.valueMACD > macd[0]?.valueMACDSignal &&
+      rsi?.value > 70 
+    )  {
+       setBuySellSignalStrength(0.3);
     } else {
+        
+   
       setBuySellSignalStrength(0.5);
     }
   };
 
   const macdChart = () => {
+    console.log("macd ->", macd);
     const reversedMacd = [...macd].reverse();
     let idx = 0;
     for (let obj of reversedMacd) {
       obj.backtrack = idx;
       idx = idx + 1;
     }
+    console.log("reverse mac->", reversedMacd);
     return reversedMacd;
   };
 
   useEffect(() => {
     buySellSignalLogic();
-  }, [macd, rsi, ma, price]);
-
-  useEffect(() => {
-    const animateGaugeChart = () => {
-      const interval = setInterval(() => {
-        if (buySellSignalStrength === 0.8) {
-          setBuySellSignalStrength((prevStrength) =>
-            prevStrength < 0.8 ? prevStrength + 0.1 : prevStrength
-          );
-        } else if (buySellSignalStrength === 0.2) {
-          setBuySellSignalStrength((prevStrength) =>
-            prevStrength < 0.2 ? prevStrength + 0.1 : prevStrength
-          );
-        } else if (buySellSignalStrength === 0.3) {
-          setBuySellSignalStrength((prevStrength) =>
-            prevStrength < 0.3 ? prevStrength + 0.1 : prevStrength
-          );
-        } else {
-          clearInterval(interval);
-        }
-      }, 200);
-    };
-
-    animateGaugeChart();
-  }, [buySellSignalStrength]);
+  }, []);
 
   return (
     <div>
@@ -93,9 +80,10 @@ const CoinSentimentAnalysis = ({
 
       <div className="grid grid-cols-2 grid-rows-2 gap-2">
         <div className="bg-white shadow-lg rounded-xl shadow-neutral-200 p-4">
-          <div className="w-full">
+          <div className="w-full ">
             <p className="text-center text-indigo-400 font-medium">NEUTRAL</p>
             <GaugeChart
+              //@ts-ignore
               id="gauge-chart"
               style={{ height: "100%", width: "100%" }}
               arcWidth={0.1}
@@ -114,7 +102,7 @@ const CoinSentimentAnalysis = ({
         </div>
 
         <div className="bg-white shadow-lg rounded-xl shadow-neutral-200 p-3">
-          <div className="h-60">
+          <div className="h-60 ">
             <VictoryChart padding={50}>
               <VictoryLine
                 interpolation="natural"
@@ -127,6 +115,7 @@ const CoinSentimentAnalysis = ({
                 x="backtrack"
                 y="valueMACD"
               />
+              {/** orange line */}
               <VictoryLine
                 interpolation="natural"
                 data={macdChart()}
@@ -150,15 +139,19 @@ const CoinSentimentAnalysis = ({
             <p className="font-medium">
               MACD Signal Value: {macd[0]?.valueMACDSignal}
             </p>
+            <p></p>
           </div>
           <div className="flex flex-row items-center text-sm">
             <p className="font-medium">MACD Value: {macd[0]?.valueMACD}</p>
+            <p></p>
           </div>
           <div className="flex flex-row items-center text-sm">
             <p className="font-medium">RSI Value: {rsi?.value} </p>
+            <p></p>
           </div>
           <div className="flex flex-row items-center text-sm">
             <p className="font-medium">200 day MA: {ma?.value}</p>
+            <p></p>
           </div>
           <div className="mt-4">
             <p className="font-base">SAFE/RISKY INDICATOR:</p>
